a1cca7b332522c2f76a2b6ed0a61a5e4ab0f5c7f
Megatron\PhazedOut <swanninger@gmail.com>
2019-06-01 15:52:12 -0500

    rpg_12

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,21 +2,45 @@
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour {
-    [SerializeField] private Transform target;
-
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float walkSpeed = 5f;
     private NavMeshAgent navMeshAgent;
-    private bool isSprinting = false;
+    
+    private bool isSprinting;
+    private Camera mainCamera;
 
     // Start is called before the first frame update
     private void Start() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     private void Update() {
-        navMeshAgent.SetDestination(target.position);
+        ProcessControls();
+    }
+
+    private void ProcessControls() {
+        if (Input.GetMouseButtonDown(0)) {
+            SetMoveSpeed(Input.GetKey("left ctrl"));
+            MoveToCursor();
+        }
+        
         if (Input.GetKeyDown("left shift")) {
             ToggleSprint();
+        }
+    }
+
+    private void SetMoveSpeed(bool sprint) {
+        navMeshAgent.speed = sprint ? runSpeed : walkSpeed;
+        this.isSprinting = sprint;
+    }
+
+    private void MoveToCursor() {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        var hasHit = Physics.Raycast(ray, out var hit);
+        if (hasHit) {
+            navMeshAgent.SetDestination(hit.point);
         }
     }
 
@@ -26,6 +50,6 @@ public class Mover : MonoBehaviour {
     }
 
     private void Sprint(bool sprint) {
-        navMeshAgent.speed = sprint ? 7f : 3.5f;
+        navMeshAgent.speed = sprint ? runSpeed : walkSpeed;
     }
 }
