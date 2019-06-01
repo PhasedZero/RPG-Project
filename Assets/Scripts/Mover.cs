a83fbfd7663d1738bbee117a0a9dad499cadfb78
Megatron\PhazedOut <swanninger@gmail.com>
2019-06-01 12:45:09 -0500

    rpg_10

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,6 +5,7 @@ public class Mover : MonoBehaviour {
     [SerializeField] private Transform target;
 
     private NavMeshAgent navMeshAgent;
+    private bool isSprinting = false;
 
     // Start is called before the first frame update
     private void Start() {
@@ -14,5 +15,17 @@ public class Mover : MonoBehaviour {
     // Update is called once per frame
     private void Update() {
         navMeshAgent.SetDestination(target.position);
+        if (Input.GetKeyDown("left shift")) {
+            ToggleSprint();
+        }
+    }
+
+    private void ToggleSprint() {
+        isSprinting = !isSprinting;
+        Sprint(isSprinting);
+    }
+
+    private void Sprint(bool sprint) {
+        navMeshAgent.speed = sprint ? 7f : 3.5f;
     }
 }
