dffd5e31b62156c3acc3371dd3b34e5a82b16075
Megatron\PhazedOut <swanninger@gmail.com>
2019-06-01 12:07:45 -0500

    rpg_9

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    private void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
