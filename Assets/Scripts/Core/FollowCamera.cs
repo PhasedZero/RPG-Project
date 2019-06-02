2ecd608b94e79533f05fb6a2eacf8563cadc8404
Megatron\PhazedOut <swanninger@gmail.com>
2019-06-02 10:06:22 -0500

    rpg_17 cleanup structure

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Core {
+    public class FollowCamera : MonoBehaviour {
+        [SerializeField] private Transform target;
+
+        // Start is called before the first frame update
+        void Start() {
+        }
+
+        // Update is called once per frame
+        private void Update() {
+            transform.position = target.position;
+        }
+    }
+}
