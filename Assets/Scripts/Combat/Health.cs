1eeab9cacb9bc51fd1f5199b6661b82a54e99204
Scott <swanninger@gmail.com>
2019-06-04 10:31:38 -0500

    rpg_37

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 namespace RPG.Combat {
     public class Health : MonoBehaviour {
         [SerializeField] private float health = 100f;
+        private Animator animator;
 
+        private void Start() {
+            animator = GetComponent<Animator>();
+        }
+        
         public void TakeDamage(float damage) {
             health = Mathf.Max(health - damage, 0f);
             print(health);
+            if (health <= 0) {
+                DeathSequence();
+            }
+        }
+        
+        private void DeathSequence() {
+            animator.SetBool("death",true);
         }
     }
 }
