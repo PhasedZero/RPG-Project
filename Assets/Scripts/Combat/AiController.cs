1cd69a04e2939aece502210f6e94976c346eaddb
Scott <swanninger@gmail.com>
2019-06-04 14:16:11 -0500

    rpg_44

---
+++ Assets/Scripts/Combat/AiController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat {
+    public class AiController : MonoBehaviour {
+        [SerializeField] private float chaseDistance = 5f;
+        
+        private GameObject player;
+
+        private void Start() {
+            player = GameObject.FindWithTag("Player");
+        }
+        
+        private void Update() {
+            if (InRange()) {
+                print(name + " In range");
+            }
+        }
+        
+        private bool InRange() {
+            var range = Vector3.Distance(transform.position, player.transform.position);
+            return range <= chaseDistance;
+        }
+    }
+}
