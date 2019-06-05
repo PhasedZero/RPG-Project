431aec348d2e4d5114cefc82d18124a8438487df
Scott <swanninger@gmail.com>
2019-06-05 15:08:16 -0500

    rpg_50

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RPG.Control {
+    public class PatrolPath : MonoBehaviour {
+        private const float waypointGizmoRadius = .3f;
+        
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.yellow;
+            
+            for (var i = 0; i < transform.childCount; i++) {
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                if (i == 0) {
+                    Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(i).position);
+                }
+                else {
+                    Gizmos.DrawLine(transform.GetChild(i - 1).position, transform.GetChild(i).position);
+                }
+            }
+        }
+    }
+}
