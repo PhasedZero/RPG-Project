f060e2aac393062ee38151a20036d2c1f79f251e
Megatron\PhazedOut <swanninger@gmail.com>
2019-06-11 16:15:45 -0500

    rpg_71.1

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -6,6 +6,7 @@ namespace RPG.SceneManagement {
     public class Portal : MonoBehaviour {
         
         [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
         
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
@@ -16,8 +17,19 @@ namespace RPG.SceneManagement {
         private IEnumerator Transition() {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal) {
+            GameObject.FindWithTag("Player").transform.position = otherPortal.spawnPoint.transform.position;
+        }
+
+        private Portal GetOtherPortal() {
+            return FindObjectOfType<Portal>();
         }
     }
 }
