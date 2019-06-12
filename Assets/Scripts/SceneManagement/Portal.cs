bb058428413f2a6e0366ca485ba6041dc6d53cbb
Scott <swanninger@gmail.com>
2019-06-12 10:37:51 -0500

    RPG_71

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -4,32 +4,37 @@ using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement {
     public class Portal : MonoBehaviour {
-        
+
         [SerializeField] private int sceneToLoad = -1;
         [SerializeField] private Transform spawnPoint;
-        
+
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 StartCoroutine(Transition());
             }
         }
-        
+
         private IEnumerator Transition() {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
-            
+
             Destroy(gameObject);
         }
 
         private void UpdatePlayer(Portal otherPortal) {
-            GameObject.FindWithTag("Player").transform.position = otherPortal.spawnPoint.transform.position;
+            GameObject.FindWithTag("Player").transform.position = otherPortal.spawnPoint.position;
         }
 
         private Portal GetOtherPortal() {
-            return FindObjectOfType<Portal>();
+            foreach (Portal portal in FindObjectsOfType<Portal>()) {
+                if (portal == this) continue;
+                return portal;
+            }
+
+            return null;
         }
     }
 }
