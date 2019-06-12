1164072b6547e86a43cddfc3d8b9cc0b7d67ff02
Scott <swanninger@gmail.com>
2019-06-12 12:55:24 -0500

    rpg_74

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement {
+    public class Fader : MonoBehaviour {
+        [SerializeField] private float timeToFade = 2f;
+        
+        private CanvasGroup canvasGroup;
+        
+        
+        private void Start() {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeIn(timeToFade));
+            
+        }
+
+        public IEnumerator FadeOut(float time) {
+            while (canvasGroup.alpha < 1) {
+                canvasGroup.alpha += Time.deltaTime / timeToFade;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time) {
+            while (canvasGroup.alpha > 0) {
+                canvasGroup.alpha -= Time.deltaTime / timeToFade;
+                yield return null;
+            }
+        }
+    }
+}
