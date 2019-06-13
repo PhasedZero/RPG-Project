92f85d0f3d02025ada45ad179d3b0bb7be7e8b93
Scott <swanninger@gmail.com>
2019-06-13 10:04:19 -0500

    rpg_81

--- Assets/Scripts/Saving/SaveSystem.cs
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace RPG.Saving {
@@ -9,17 +10,24 @@ namespace RPG.Saving {
             var path = GetPathFromSaveFile(saveFile);
             print("Saving to " + path);
             
-            var stream = File.Open(path, FileMode.Create);
-            var bytes = new byte[]{0xc2, 0xa1, 72, 111, 108, 97, 32, 77, 117, 110, 100, 111, 33};
-            stream.Write(bytes, 0, bytes.Length);
-            stream.Close();
+            using (var stream = File.Open(path, FileMode.Create)) {
+                var bytes = Encoding.UTF8.GetBytes("¡Hola Mundo!");
+                stream.Write(bytes, 0, bytes.Length);
+            }
         }
-        
+
         public void Load(string saveFile) {
             var path = GetPathFromSaveFile(saveFile);
             print("Loading from " + path);
+            
+            using (var stream = File.Open(path, FileMode.Open)) {
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer,0,buffer.Length);
+                print(Encoding.UTF8.GetString(buffer));
+            }
+
         }
-        
+
         private string GetPathFromSaveFile(string saveFile) {
             return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
