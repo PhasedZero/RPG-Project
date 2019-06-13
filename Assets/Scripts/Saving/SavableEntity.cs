f4c65b54ec6641d91a51211cc3bb4247889eb9e8
Scott <swanninger@gmail.com>
2019-06-13 13:52:58 -0500

    rpg_85

--- Assets/Scripts/Saving/SavableEntity.cs
+++ Assets/Scripts/Saving/SavableEntity.cs
@@ -1,18 +1,28 @@
+using System;
 using UnityEngine;
 
 namespace RPG.Saving {
+    [ExecuteAlways]
     public class SavableEntity : MonoBehaviour {
-         public string GetUniqueId() {
-             return "";
-         }
-         
-         public object CaptureState() {
-             print("Capturing state for " + GetUniqueId());
-             return null;
-         }
-         
-         public void RestoreState(object state) {
-             print("Restoring state for " + GetUniqueId());
-         }
+        [SerializeField] private string uniqueId = Guid.NewGuid().ToString();
+
+        public string GetUniqueId() {
+            return uniqueId;
+        }
+
+        public object CaptureState() {
+            print("Capturing state for " + GetUniqueId());
+            return null;
+        }
+
+        public void RestoreState(object state) {
+            print("Restoring state for " + GetUniqueId());
+        }
+        
+        private void Update() {
+            if (Application.isPlaying) return;
+            
+            print("Editing....");
+        }
     }
 }
