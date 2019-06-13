a4f8196e77d26c0bca55f2ce62bfb02a0b95386c
Scott <swanninger@gmail.com>
2019-06-13 08:50:37 -0500

    rpg_80

--- Assets/Scripts/Saving/SaveSystem.cs
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -6,15 +6,22 @@ namespace RPG.Saving {
     public class SaveSystem : MonoBehaviour {
 
         public void Save(string saveFile) {
-            print("Saving to " + GetPathFromSaveFile("save"));
+            var path = GetPathFromSaveFile(saveFile);
+            print("Saving to " + path);
+            
+            var stream = File.Open(path, FileMode.Create);
+            var bytes = new byte[]{0xc2, 0xa1, 72, 111, 108, 97, 32, 77, 117, 110, 100, 111, 33};
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Close();
         }
         
         public void Load(string saveFile) {
-            print("Loading from " + GetPathFromSaveFile("save"));
+            var path = GetPathFromSaveFile(saveFile);
+            print("Loading from " + path);
         }
         
-        private string GetPathFromSaveFile(string savefile) {
-            return Path.Combine(Application.persistentDataPath, savefile + ".sav");
+        private string GetPathFromSaveFile(string saveFile) {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
     }
 }
