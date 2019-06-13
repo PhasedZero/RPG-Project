9c32b8bd8821d60914721fbb4dd9ff0365c8b149
Scott <swanninger@gmail.com>
2019-06-13 11:29:23 -0500

    rpg_83

--- Assets/Scripts/Saving/SaveSystem.cs
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -1,6 +1,6 @@
 using System;
 using System.IO;
-using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace RPG.Saving {
@@ -16,9 +16,11 @@ namespace RPG.Saving {
             print("Saving to " + path);
             
             using (var stream = File.Open(path, FileMode.Create)) {
-                byte[] buffer = SerializeVector(playerTransform.position);
                 
-                stream.Write(buffer, 0, buffer.Length);
+                var formatter = new BinaryFormatter();
+                var position = new SerializableVector3(playerTransform.position);
+                formatter.Serialize(stream, position);
+                
             }
         }
         
@@ -28,10 +30,10 @@ namespace RPG.Saving {
             print("Loading from " + path);
             
             using (var stream = File.Open(path, FileMode.Open)) {
-                var buffer = new byte[stream.Length];
-                stream.Read(buffer,0,buffer.Length);
-                playerTransform.position = DeSerializeVector(buffer);
-                
+                var formatter = new BinaryFormatter();
+
+                var position = (SerializableVector3) formatter.Deserialize(stream);
+                playerTransform.position = position.ToVector3();
             }
         }
 
---
+++ Assets/Scripts/Saving/SerializableVector3.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace RPG.Saving {
+    [System.Serializable]
+    public class SerializableVector3 {
+        private float x, y, z;
+
+        public SerializableVector3(Vector3 vector3) {
+            x = vector3.x;
+            y = vector3.y;
+            z = vector3.z;
+        }
+
+        public Vector3 ToVector3() {
+            return new Vector3(x, y, z);
+        }
+    }
+}
