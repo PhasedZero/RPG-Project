1e4a33db357aa7f2e22a0ad161d6ae43334d05c6
Scott <swanninger@gmail.com>
2019-06-13 08:21:55 -0500

    rpg_79

--- Assets/Scripts/Saving/SaveSystem.cs
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -1,14 +1,20 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 namespace RPG.Saving {
     public class SaveSystem : MonoBehaviour {
-        
+
         public void Save(string saveFile) {
-            print("Saving to " + saveFile);
+            print("Saving to " + GetPathFromSaveFile("save"));
         }
         
         public void Load(string saveFile) {
-            print("Loading from " + saveFile);
+            print("Loading from " + GetPathFromSaveFile("save"));
+        }
+        
+        private string GetPathFromSaveFile(string savefile) {
+            return Path.Combine(Application.persistentDataPath, savefile + ".sav");
         }
     }
 }
