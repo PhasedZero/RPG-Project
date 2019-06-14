c6f8e31c08b5a703fe9f3abc0d238194657f50e4
Scott <swanninger@gmail.com>
2019-06-14 10:04:02 -0500

    rpg_88

--- Assets/Scripts/Saving/SaveSystem.cs
+++ Assets/Scripts/Saving/SaveSystem.cs
@@ -9,28 +9,38 @@ namespace RPG.Saving {
     public class SaveSystem : MonoBehaviour {
         
         public void Save(string saveFile) {
+
+            SaveFile(saveFile, CaptureState());
+        }
+        
+        private void SaveFile(string saveFile, object state) {
             var path = GetPathFromSaveFile(saveFile);
             print("Saving to " + path);
             
             using (var stream = File.Open(path, FileMode.Create)) {
                 
                 var formatter = new BinaryFormatter();
-                formatter.Serialize(stream, CaptureState());
+                formatter.Serialize(stream, state);
             }
         }
-        
+
         public void Load(string saveFile) {
+            
+            RestoreState(LoadFile(saveFile));
+        }
+        
+        private Dictionary<string, object> LoadFile(string saveFile) {
             var path = GetPathFromSaveFile(saveFile);
             print("Loading from " + path);
             
             using (var stream = File.Open(path, FileMode.Open)) {
                 var formatter = new BinaryFormatter();
 
-                RestoreState(formatter.Deserialize(stream));
+                return (Dictionary<string, object>) formatter.Deserialize(stream);
             }
         }
 
-        private object CaptureState() {
+        private static Dictionary<string, object> CaptureState() {
             var state = new Dictionary<string, object>();
             foreach (var savable in FindObjectsOfType<SavableEntity>()) {
                 state[savable.GetUniqueId()] = savable.CaptureState();
@@ -38,10 +48,9 @@ namespace RPG.Saving {
             return state;
         }
         
-        private void RestoreState(object state) {
-            var dictionary = (Dictionary<string, object>) state;
+        private static void RestoreState(IReadOnlyDictionary<string, object> state) {
             foreach (var savable in FindObjectsOfType<SavableEntity>()) {
-                savable.RestoreState(dictionary[savable.GetUniqueId()]);
+                savable.RestoreState(state[savable.GetUniqueId()]);
             }
         }
 
