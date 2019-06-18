9c99adec6bc0c382689de391539965be0eb0c6d0
Megatron\PhazedOut <swanninger@gmail.com>
2019-06-18 18:26:15 -0500

    added 3rd person camera

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,12 +1,18 @@
-﻿using RPG.Combat;
+﻿using Cinemachine;
+using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control {
     public class PlayerController : MonoBehaviour {
-        [Range(0,1)]
-        [SerializeField] private float walkSpeedFraction = 0.4f;
+        [Range(0, 1)] [SerializeField] private float walkSpeedFraction = 0.4f;
+
+        [SerializeField] private CinemachineVirtualCamera thirdPersonCamera;
+        [SerializeField] private CinemachineVirtualCamera oneThirdCamera;
+        [SerializeField] private float cameraSpeed = 1f;
+        private bool isThirdPerson = true;
+
         private float moveSpeedFraction = 1f;
 
         private Camera mainCamera;
@@ -27,17 +33,46 @@ namespace RPG.Control {
                 ToggleSprint();
             }
 
+            if (Input.GetKeyDown(KeyCode.F)) {
+                SwitchCamera();
+            }
+
+            if (isThirdPerson && Input.GetMouseButton(1)) {
+                ProcessCameraMovement();
+            }
+
             if (InteractWithCombat()) return;
 
             if (InteractWithMovement()) return;
         }
 
+        private void ProcessCameraMovement() {
+            var rotation = thirdPersonCamera.transform.localEulerAngles;
+            var xAxis = Input.GetAxis("Mouse Y") * cameraSpeed + rotation.x;
+            var yAxis = Input.GetAxis("Mouse X") * cameraSpeed + rotation.y;
+
+            thirdPersonCamera.transform.localRotation = Quaternion.Euler(xAxis, yAxis, 0);
+        }
+
+        private void SwitchCamera() {
+            if (isThirdPerson) {
+                thirdPersonCamera.enabled = false;
+                oneThirdCamera.enabled = true;
+                isThirdPerson = false;
+            }
+            else {
+                thirdPersonCamera.enabled = true;
+                oneThirdCamera.enabled = false;
+                isThirdPerson = true;
+            }
+        }
+
         private bool InteractWithCombat() {
             var hits = Physics.RaycastAll(GetRay());
-            
+
             foreach (var hit in hits) {
                 var target = hit.transform.GetComponent<CombatTarget>();
-                
+
                 if (!target) continue;
                 if (!fighter.CanAttack(target.gameObject)) continue;
 
@@ -60,13 +95,12 @@ namespace RPG.Control {
             }
 
             return true;
-
         }
 
         private Ray GetRay() {
             return mainCamera.ScreenPointToRay(Input.mousePosition);
         }
-        
+
         private void ToggleSprint() {
             moveSpeedFraction = moveSpeedFraction < 1 ? 1 : walkSpeedFraction;
             mover.UpdateMovementSpeed(moveSpeedFraction);
